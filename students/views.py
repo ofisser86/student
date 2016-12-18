d0f24a860ba88fc46df5c1de4f3c0c82f929d689
greta <greta@ubuntu.greta>
2016-12-18 10:40:58 +0200

    Add dinamic links

--- students/views.py
+++ students/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Views for students
+
+
+def students_list(request):
+    return render(request, 'students/students_list.html', {})
+
+
+def students_add(request):
+    return HttpResponse('<h1>Student Add Form</h1>')
+
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Student Edit Form %s</h1>' % sid)
+
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Student Delete Form %s </h1>' % sid)
+
+# Views for Groups
+
+
+def groups_list(request):
+    return HttpResponse('<h1>Groups Listing</h1>')
+
+
+def groups_add(request):
+    return HttpResponse('<h1>Groups Listing</h1>')
+
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Groups Listing %s</h1>' % gid)
+
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Groups Listing %s</h1>' % gid)
+
+
