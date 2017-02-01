1ebdc671f1aa2e5ad4281ef248a755060c891f4e
ofisser86 <ofisser86@gmail.com>
2017-02-01 22:52:22 +0200

    create Admin form with Django form

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls.static import static
-from students.views import students, groups, journal, exams
+from students.views import students, groups, journal, exams, contact_admin
 from .settings import MEDIA_ROOT, DEBUG
 
 urlpatterns = [
@@ -46,6 +46,10 @@ urlpatterns = [
     url(r'^exams/add/$', exams.exams_add, name='exams_add'),
     url(r'^exams/(?P<eid>\d+)/edit/$', exams.exams_edit, name='exam_edit'),
     url(r'^exams/(?P<eid>\d+)/delete/$', exams.exams_delete, name='delete'),
+
+    # Form contact admin
+
+    url(r'^contact-admin/$', contact_admin.contact_admin, name='contact_admin'),
 
     url(r'^admin/', admin.site.urls),
 
