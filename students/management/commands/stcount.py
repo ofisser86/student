1cb2e6f9bd81cb7020bbcfc10a7790877e37b06c
ofisser86 <ofisser86@gmail.com>
2017-07-07 15:32:59 +0300

    django-admin stcount module

--- students/management/commands/stcount.py
+++ students/management/commands/stcount.py
@@ -1,12 +1,17 @@
 from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
 
-from students.models import Student
+from students.models import Student, Group
 
 
 class Command(BaseCommand):
     args = '<model_name model_name ...>'
     help = "Prints to console number of students related objects in a database"
 
+    models = (('student', Student), ('group', Group), ('user', User))
+
     def handle(self, *args, **options):
-        if 'student' in args:
-            self.stdout.write('Number of student in DB = %d' % Student.objects.count())
+        for name, model in self.models:
+            # if name in args:
+            #     self.stdout.write('Number of %ss in DB = %d' % (name, model.objects.count()))
+            self.stdout.write('Number of %ss in DB = %d' % (name, model.objects.count()))
