0ec3227c1f80d1603142f02f02502ab2a054356e
ofisser86 <ofisser86@gmail.com>
2017-07-09 15:53:02 +0300

    Add group_add.html template

--- students/views/groups.py
+++ students/views/groups.py
@@ -37,7 +37,7 @@ def groups_list(request):
 
 
 def groups_add(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    return render(request, 'students/group_add.html')
 
 
 def groups_edit(request, gid):
